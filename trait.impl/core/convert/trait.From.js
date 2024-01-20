1d80bacc8800c064fe577b385300266eff933c8b
Virv12 <55024474+Virv12@users.noreply.github.com>
2024-01-20 02:31:42 +0000

    Deploying to gh-pages from @ algorithm-ninja/pixie@8563e75d88c800be554ad81e23884aff6505505e 🚀

---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"pixie_shared":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"pixie_shared/struct.Address.html\" title=\"struct pixie_shared::Address\">Address</a>&gt; for <a class=\"struct\" href=\"https://doc.rust-lang.org/1.75.0/core/net/socket_addr/struct.SocketAddrV4.html\" title=\"struct core::net::socket_addr::SocketAddrV4\">SocketAddrV4</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
